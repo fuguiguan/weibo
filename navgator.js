f50ecfcd8e7b02d76311545d2d825615d7f74beb
傅贵观 <fuguiguan@fuguiguandeMacBook-Pro.local>
2019-04-26 21:34:49 +0800

    nothing

--- navgator.js
+++ navgator.js
@@ -2,10 +2,10 @@
  * @Author: fuguiguan
  * @Date: 2019-03-03 15:22:24
  * @Last Modified by: fuguiguan
- * @Last Modified time: 2019-04-21 11:43:26
+ * @Last Modified time: 2019-04-26 21:33:17
  */
 import React from 'react';
-import { createAppContainer, createBottomTabNavigator, } from 'react-navigation';
+import { createAppContainer, createBottomTabNavigator, createStackNavigator} from 'react-navigation';
 import Icon from 'react-native-vector-icons/Ionicons';
 import MsgIcon from 'react-native-vector-icons/FontAwesome'
 import Home from './src/components/Home/Home';
